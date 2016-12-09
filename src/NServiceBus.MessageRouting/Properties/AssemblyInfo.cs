27e0e2ff66cf5b0864ab7803cc556bf3d9cd49eb
Simon Cropp <simon.cropp@gmail.com>
2016-12-09 17:26:24 +1100

    use Serializer is test

--- src/NServiceBus.MessageRouting/Properties/AssemblyInfo.cs
+++ src/NServiceBus.MessageRouting/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 
 [assembly: AssemblyTitle("NServiceBus.MessageRouting")]
 [assembly: AssemblyDescription("Messaging routing patterns for NServiceBus")]
@@ -7,3 +8,4 @@
 [assembly: AssemblyCopyright("Copyright ©  2013-2016")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: InternalsVisibleTo("NServiceBus.MessageRouting.UnitTests")]
